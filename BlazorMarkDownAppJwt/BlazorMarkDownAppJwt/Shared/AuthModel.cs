a2a2c63e6ceda0d2cd62399fe318c941dd8df753
calexandre <calexandre@talentsoft.com>
2022-08-21 22:01:36 +0200

    Continue jwt authent

--- BlazorMarkDownAppJwt/BlazorMarkDownAppJwt/Shared/AuthModel.cs
+++ BlazorMarkDownAppJwt/BlazorMarkDownAppJwt/Shared/AuthModel.cs
@@ -16,18 +16,18 @@ namespace BlazorMarkDownAppJwt.Shared
 		[EmailAddress(ErrorMessage = "Email address is not valid.")]
 		public string email { get; set; } // NOTE: email will be the username, too
 
-		[Required(ErrorMessage = "First Name is required.")]
-		public string firstName { get; set; } // NOTE: for the moment not used
-
-		[Required(ErrorMessage = "Last Name is required.")]
-		public string lastName{ get; set; } // NOTE: for the moment not used
-
 		[Required(ErrorMessage = "Password is required.")]
 		[DataType(DataType.Password)]
 		public string password { get; set; }
 	}
 	public class RegModel : LoginModel
 	{
+		[Required(ErrorMessage = "First Name is required.")]
+		public string firstName { get; set; } // NOTE: for the moment not used
+
+		[Required(ErrorMessage = "Last Name is required.")]
+		public string lastName { get; set; } // NOTE: for the moment not used
+
 		[Required(ErrorMessage = "Confirm password is required.")]
 		[DataType(DataType.Password)]
 		[Compare("password", ErrorMessage = "Password and confirm password do not match.")]
