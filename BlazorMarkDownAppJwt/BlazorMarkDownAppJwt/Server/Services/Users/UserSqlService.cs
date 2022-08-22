37eb9393ab98c18db26281457c69bec4ebb5fded
calexandre <calexandre@talentsoft.com>
2022-08-22 12:02:51 +0200

    Add UserSqlService

---
+++ BlazorMarkDownAppJwt/BlazorMarkDownAppJwt/Server/Services/Users/UserSqlService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorMarkDownAppJwt.Server.Entities;
+
+namespace BlazorMarkDownAppJwt.Server.Services.Users
+{
+	public class UserSqlService : IUserService
+	{
+		public Task<User?> AddUser(User user)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<User?> AuthenticateUser(string? email, string? password)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
